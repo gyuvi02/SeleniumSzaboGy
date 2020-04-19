8d13c905b130aa112d29b66a2b529a04304ac532
gyuvi02 <gyulaother@gmail.com>
2020-04-19 10:56:36 +0200

    First 4 Scenarios

---
+++ src/test/java/com/gyula/StepDefinitions.java
@@ -0,0 +1,146 @@
+package com.gyula;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en_scouse.An;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+    static WebDriver driver;
+
+    static {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Given("The homepage is opened")
+    public void theHomePageIsOpened() {
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+        driver.manage().window().maximize();
+    }
+
+    @Given("Email address is added")
+    public void emailAddressIsAdded() {
+        driver.findElement(By.id("email_create")).sendKeys("valami@myemail.hu");
+    }
+
+    @And("Registration button is clicked")
+    public void registrationButtonIsClicked() {
+        driver.findElement(By.id("SubmitCreate")).click();
+    }
+
+
+    @An("The firstnamefield is filled")
+    public void theFirstnamefieldIsFilled() {
+        driver.findElement(By.id("customer_firstname")).sendKeys("Gyula");
+    }
+
+    @And("The lastnamefield is filled")
+    public void theLastnamefieldIsFilled() {
+        driver.findElement(By.id("customer_lastname")).sendKeys("Szabo");
+    }
+
+    @And("The passwordfield is filled")
+    public void thePasswordfieldIsFilled() {
+        driver.findElement(By.id("passwd")).sendKeys("password");
+    }
+
+    @And("Gender added")
+    public void genderAdded() {
+        driver.findElement(By.id("id_gender1")).click();
+    }
+
+    @And("Birthday day added")
+    public void birthdayDayAdded() {
+        driver.findElement(By.id("days")).sendKeys("14");
+    }
+
+    @And("Birthday month added")
+    public void birthdayMonthAdded() {
+        driver.findElement(By.id("months")).sendKeys("July");
+    }
+
+    @And("Birthday year added")
+    public void birthdayYearAdded() {
+        driver.findElement(By.id("years")).sendKeys("1980");
+    }
+
+    @And("Newsletter clicked")
+    public void newsletterClicked() {
+        driver.findElement(By.id("newsletter")).click();
+    }
+
+    @And("Company name added")
+    public void companyNameAdded() {
+        driver.findElement(By.id("company")).sendKeys("MyCompany");
+    }
+
+    @And("Address added")
+    public void addressAdded() {
+        driver.findElement(By.id("address1")).sendKeys("Mandula utca 1");
+    }
+
+    @And("City added")
+    public void cityAdded() {
+        driver.findElement(By.id("city")).sendKeys("Debrecen");
+    }
+
+    @And("State added")
+    public void stateAdded() {
+        driver.findElement(By.id("id_state")).sendKeys("Hawaii");
+    }
+
+    @And("Postal code added")
+    public void postalCodeAdded() {
+        driver.findElement(By.id("postcode")).sendKeys("54321");
+    }
+
+    @And("Register button clicked")
+    public void registerButtonClicked() {
+        driver.findElement(By.id("submitAccount")).click();
+    }
+
+    @Then("A phone number missing error is displyed")
+    public void aPhoneNumberMissingErrorIsDisplyed() {
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"center_column\"]/div/ol/li"));
+        Assert.assertNotEquals(0, elements.size());
+        Assert.assertEquals("You must register at least one phone number.", elements.get(0).getText());
+    }
+
+    @Given("The {string} is filled with {string}")
+    public void theEmailfieldIsFilledWithValue(String field, String email) {
+        driver.findElement(By.id(field)).sendKeys(email);
+    }
+
+    @Then("The create account button is clicked")
+    public void theCreateAccountButtonIsClicked() {
+        driver.findElement(By.id("SubmitCreate")).click();
+    }
+
+    @Given("Invalid email address is added")
+    public void invalidEmailAddressIsAdded() {
+        driver.findElement(By.id("email_create")).sendKeys("eznememail");
+    }
+
+    @Then("Invalid email address error displayed")
+    public void invalidEmailAddressErrorDisplayed() {
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"create_account_error\"]/ol/li"));
+        Assert.assertNotEquals(0, elements.size());
+        Assert.assertEquals("Invalid email address.", elements.get(0).getText());
+
+    }
+}
---
+++ src/test/resources/com/gyula/GyulaGherkin.feature
@@ -0,0 +1,40 @@
+Feature: YourLogo registration
+
+  Background:
+    Given The homepage is opened
+
+  Scenario Outline:
+    Given The '<emailfield>' is filled with '<value>'
+    Then The create account button is clicked
+    Examples:
+      | emailfield          | value                 |
+      | email_create        | valami1@myemail.hu    |
+      | email_create        | valamimas@myemail.hu  |
+
+  Scenario:
+    Given Invalid email address is added
+    And Registration button is clicked
+    Then Invalid email address error displayed
+
+
+  Scenario:
+    Given Email address is added
+    And Registration button is clicked
+    And The firstnamefield is filled
+    And Gender added
+    And The lastnamefield is filled
+    And The passwordfield is filled
+    And Birthday day added
+    And Birthday month added
+    And Birthday year added
+    And Newsletter clicked
+    And Company name added
+    And Address added
+    And City added
+    And State added
+    And Postal code added
+    And Register button clicked
+    Then A phone number missing error is displyed
+
+
+
